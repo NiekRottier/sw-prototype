a83ff485e42971b404bb3970aff2cd2206dbd38e
NiekRottier <niekrottier@gmail.com>
2024-01-06 23:42:27 +0100

    Precache selected URLs and cache on frontend navigation to new page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  cacheStartUrl: true,
+  cacheOnFrontendNav: true,
+  workboxOptions: {
+    additionalManifestEntries: [
+      '/',
+      '/new-page',
+      '/new-page/deeper-page',
+      '/manifest.webmanifest',
+    ]
+  }
 });
 
 const nextConfig = withPWA({})
